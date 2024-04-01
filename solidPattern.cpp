d9b843dbed76f3c2ce432be31598e973cd3c50e8
faize700 <gmfaize123@gmail.com>
2024-04-01 20:34:17 +0530

    Ques 1

---
+++ solidPattern.cpp
@@ -0,0 +1,26 @@
+// Print Solid Pattern of Asterisk (*) in C++
+// ******
+// ******
+// ******
+// ******
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int rows, cols;
+    cout<<"Enter Number of Rows"<<endl;
+    cin>>rows;
+    cout<<"Enter Number of Columns"<<endl;
+    cin>>cols;
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
